526ec6542abcc0c890301c78d1e0be3d007e75b1
Sergey Ryzhov <sergey.ryzhov@firstlinesoftware.com>
2022-03-25 09:43:44 +0100

    Enable catalog functionality

--- FLS.CoffeeDesk/Infrastructure/SiteInitialization.cs
+++ FLS.CoffeeDesk/Infrastructure/SiteInitialization.cs
@@ -1,22 +1,32 @@
 ﻿using EPiServer.Commerce.Routing;
 using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FLS.CoffeeDesk.Infrastructure
 {
     [InitializableModule]
     [ModuleDependency(typeof(EPiServer.Commerce.Initialization.InitializationModule))]
-    public class SiteInitialization : IInitializableModule
+    public class SiteInitialization : IInitializableModule, IConfigurableModule
     {
         public void Initialize(InitializationEngine context)
         {
             CatalogRouteHelper.MapDefaultHierarchialRouter(false);
+
+            var enableCatalogRoot = context.Locate.Advanced.GetService<EnableCatalogRoot>();
+            enableCatalogRoot.SetCatalogAccessRights();
         }
 
         public void Preload(string[] parameters) { }
 
         public void Uninitialize(InitializationEngine context)
         {
+        }
+
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            context.Services.AddSingleton<EnableCatalogRoot>();
         }
     }
 }
